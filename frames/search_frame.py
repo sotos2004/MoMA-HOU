6e4d3f96f239f7f0aa4929fdf4183067254dc91e
Bernikolas <bernikolas@gmail.com>
2024-06-07 17:00:25 +0300

    Update search_frame.py

--- frames/search_frame.py
+++ frames/search_frame.py
@@ -1,6 +1,21 @@
-import datetime
-import tkinter as tk
-from tkinter import ttk
 import customtkinter as ctk
-from PIL import Image, ImageTk
-from customization.color_styles import *
+
+
+class SearchFrame(ctk.CTkScrollableFrame):
+
+    def __init__(self, container, *args, **kwargs):
+        """
+        Αρχικοποίηση της κλάσης
+        """
+        super().__init__(container, *args, **kwargs)
+        # Δημιουργία frame και widgets
+        self.searchFrame = ctk.CTkFrame(container, border_width=20)
+        self.searchFrame.grid(row=1, column=1, columnspan=3, sticky="NSEW")
+        self.searchFrame.columnconfigure(0, weight=1, uniform='f3')
+        self.searchFrame.rowconfigure(0, weight=1, uniform='f3')
+
+        self.title_label = ctk.CTkLabel(self.searchFrame,
+                                        text="Αναζήτηση",
+                                        anchor="center",
+                                        font=("Arial", 16, "bold"))
+        self.title_label.pack(fill="x", pady=10, padx=20)
