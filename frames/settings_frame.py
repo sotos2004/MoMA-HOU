0a215e41c959f683c54d70f4ffea2830c7969a07
Bernikolas <bernikolas@gmail.com>
2024-06-07 16:27:28 +0300

    added comments

--- frames/settings_frame.py
+++ frames/settings_frame.py
@@ -1,80 +1,152 @@
-from tkinter import messagebox
 import customtkinter as ctk
 import configparser
-import os
+import sys
+import moma_class as mc
+from tkinter import messagebox
+
+sys.path.append('../')
+configLocation = 'DATA/config.ini'
 
 class SettingsFrame(ctk.CTkScrollableFrame):
 
     def __init__(self, container, *args, **kwargs):
+        """
+        Αρχικοποίηση της κλάσης
+        """
         super().__init__(container, *args, **kwargs)
-        self.configLocation = 'DATA/config.ini'
+        # Αρχικοποίηση μεταβλητών
+        self.checkboxValue = ctk.StringVar()
+        self.md = mc.MoMA()
+        self.configLocation = configLocation
+
+        # Λήψη δεδομένων παραμετροποίησης
+        config = self.readConfig()
+
+        # Δημιουργία frame και widgets
         self.settingsFrame = ctk.CTkFrame(container, border_width=20)
         self.settingsFrame.grid(row=1, column=1, columnspan=3, sticky="NSEW")
-        self.settingsFrame.columnconfigure(0, weight=1, uniform = 'f3')
-        self.settingsFrame.rowconfigure(0, weight=1, uniform = 'f3')
+        self.settingsFrame.columnconfigure(0, weight=1, uniform='f3')
+        self.settingsFrame.rowconfigure(0, weight=1, uniform='f3')
 
-        self.title_label = ctk.CTkLabel(self.settingsFrame, text="Ρυθμίσεις", anchor="center",font=("Arial", 16, "bold"))
-        self.title_label.pack(fill="x", pady=5, padx=20)
+        self.titleLabel = ctk.CTkLabel(self.settingsFrame,
+                                        text="Ρυθμίσεις",
+                                        anchor="center",
+                                        font=("Arial", 16, "bold"))
+        self.titleLabel.pack(fill="x", pady=5, padx=20)
 
         self.dbPathLabel = ctk.CTkLabel(self.settingsFrame, text="Τοποθεσία Βάσης")
-        self.dbPathLabel.pack( padx=20)
+        self.dbPathLabel.pack(padx=20)
+
         self.dbPathEntry = ctk.CTkEntry(self.settingsFrame, width=300)
         self.dbPathEntry.pack(padx=20, pady=5)
+        self.dbPathEntry.insert(0, config.get('SETTINGS', 'databasepath'))
 
         self.webUrlLabel = ctk.CTkLabel(self.settingsFrame, text="Τοποθεσία διαδικτύου (Url)")
-        self.webUrlLabel.pack( padx=20)
+        self.webUrlLabel.pack(padx=20)
+
         self.webUrlEntry = ctk.CTkEntry(self.settingsFrame, width=300)
         self.webUrlEntry.pack(padx=20, pady=5)
+        self.webUrlEntry.insert(0, config.get('SETTINGS', 'webdataurl'))
 
         self.dbTypeLabel = ctk.CTkLabel(self.settingsFrame, text="Είδος Βάσης Δεδομένων")
         self.dbTypeLabel.pack(padx=20)
-        self.dbTypeCombo = ctk.CTkComboBox(self.settingsFrame, values=["SqLite3", "MySQL"])
+
+        self.dbTypeCombo = ctk.CTkComboBox(self.settingsFrame, values=["SqLite3"])
+        self.dbTypeCombo.set(config.get('SETTINGS', 'databasetype'))
         self.dbTypeCombo.pack(padx=20, pady=5)
 
         self.buttonFrame = ctk.CTkFrame(self.settingsFrame)
         self.buttonFrame.pack(pady=10)
-        self.saveButton = ctk.CTkButton(self.buttonFrame, text="Save", command=self.saveConfig())
+
+        self.saveButton = ctk.CTkButton(self.buttonFrame, text="Αποθήκευση", command=lambda: self.__saveConfig())
         self.saveButton.pack(side="left", padx=10)
-        self.resetButton = ctk.CTkButton(self.buttonFrame, text="Reset", command=self.resetFields())
-        self.resetButton.pack(side="left", padx=10)
 
-        self.__resetFields()
+        self.resetButton = ctk.CTkButton(self.buttonFrame, text="Επαναφορά", command=lambda: self.__resetFields())
+        self.resetButton.pack(side="left", padx=20)
 
-    def readConfig(self):
+        self.advancedTitleLabel = ctk.CTkLabel(self.settingsFrame,
+                                        text="Προχωρημένες Ρυθμίσεις",
+                                        anchor="center",
+                                        font=("Arial", 16, "bold"))
+        self.advancedTitleLabel.pack(fill="x", pady=10, padx=20)
+
+        self.advancedButtonFrame = ctk.CTkFrame(self.settingsFrame)
+        self.advancedButtonFrame.pack(pady=10)
+
+        self.resetDbButton = ctk.CTkButton(self.advancedButtonFrame, text="Αρχικοποίηση βάσης",
+                                           command=lambda: self.__resetDatabase())
+        self.resetDbButton.pack(side="left", padx=10)
+
+        self.insertDataButton = ctk.CTkButton(self.advancedButtonFrame, text="Εισαγωγή δεδομένων απο web",
+                                                 command=lambda: self.__importData(self.checkbox.get()))
+        self.insertDataButton.pack(side="left", padx=10)
+
+        # Το παρακάτω checkbox μας επιτρέπει να περιορίσουμε την εισαγωγή των δεδομένων
+        # σύμφωνα με την εκφώνηση της εργασίας
+        self.checkbox = ctk.CTkCheckBox(master=self.settingsFrame,
+                                        text="Εισαγωγή μόνο 'Painting & Sculpture' & 'Media and Performance' τμήματα.",
+                                        onvalue="yes", offvalue="no", variable=self.checkboxValue)
+        self.checkbox.select("yes")
+        self.checkbox.pack(padx=10, pady=10)
+
+    @staticmethod
+    def readConfig():
+        """
+        Διαβάζει της παραμέτρους της εφαρμογής
+        :return:
+        """
         config = configparser.ConfigParser()
-        if not os.path.exists(self.configLocation):
-            config['SETTINGS'] = {
-                'databasepath': '',
-                'webdataurl': '',
-                'databasetype': 'SqLite3'
-            }
-            with open(self.configLocation, 'w') as configFile:
-                config.write(configFile)
-        else:
-            config.read(self.configLocation)
-            print('read')
-            print(config['SETTINGS']['databasepath'])
-            print(config['SETTINGS']['databasepath'])
+        config.read(configLocation)
         return config
 
     def __saveConfig(self):
+        """
+        Ενημερώνει το αρχείο με της παραμέτρους της εφαρμογής
+        :return: void
+        """
         config = configparser.ConfigParser()
         config['SETTINGS'] = {
             'databasepath': self.dbPathEntry.get(),
             'webdataurl': self.webUrlEntry.get(),
             'databasetype': self.dbTypeCombo.get()
         }
-        print(self.webUrlEntry.get())
-        print(config['SETTINGS']['databasetype'])
         with open(self.configLocation, 'w') as configfile:
             config.write(configfile)
-            #messagebox.showinfo('ΜoMA Navigator for EAP-2024','Επιτυχής ενημέρωση')
-    def __resetFields(self):
-        config = self.readConfig()
-        print(config['SETTINGS']['databasePath'])
-        self.dbPathEntry.delete(0, ctk.END)
-        self.dbPathEntry.insert(0, config['SETTINGS']['databasepath'])
-        self.webUrlEntry.delete(0, ctk.END)
-        self.webUrlEntry.insert(0, config['SETTINGS']['webdataurl'])
-        self.dbTypeCombo.set(config['SETTINGS']['databasetype'])
+        return
 
+    def __resetFields(self):
+        """
+        Επαναφέρει τα πεδία στις τιμές από το αρχείο της εφαρμογής
+        :return:
+        """
+        config = self.readConfig()
+        self.dbPathEntry.delete(0, ctk.END)
+        self.dbPathEntry.insert(0, config.get('SETTINGS', 'databasepath'))
+        self.webUrlEntry.delete(0, ctk.END)
+        self.webUrlEntry.insert(0, config.get('SETTINGS', 'webdataurl'))
+        self.dbTypeCombo.set(config.get('SETTINGS', 'databasetype'))
+        return
+
+    def __resetDatabase(self):
+        """
+        Αρχικοποίηση της βάσης με ανατροφοδότηση απο messageboxes
+        :return:
+        """
+        response = messagebox.askyesno("MoMA Navigator",
+                                       "Είστε σίγουρος πως θέλετε να κάνετε αρχικοποίηση της βάσης;\n"
+                                       "Προσοχή! Όλα τα δεδομένα της θα διαγραφούν!")
+        if response:
+            self.settingsFrame.after(100, self.md.createDb())
+            messagebox.showinfo("MoMA navigator", "Η διαδικασία ολοκληρώθηκε!")
+
+    def __importData(self, value):
+        """
+        Εισαγωγή δεδομένων στη βάση με ανατροφοδότηση απο messageboxes
+        :return:
+        """
+        response = messagebox.askyesno("MoMA Navigator",
+                                           "Είστε σίγουρος πως θέλετε να κάνετε εισαγωγή δεδομένων στη βάση;\n"
+                                           "Προσοχή! Η διαδικασία απαιτεί σύνδεση στο Διαδίκτυο και αρκετό χρόνο!")
+        if response:
+            self.settingsFrame.after(100, self.md.importData(value))
+            messagebox.showinfo("MoMA navigator", "Η διαδικασία ολοκληρώθηκε!")
